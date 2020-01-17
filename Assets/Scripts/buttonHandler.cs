0f562d9452c973d77c5421f53e6b45a0d6a353cc
francoisauxietre <f.auxietre@gmail.com>
2020-01-17 12:51:40 +0100

    Départ

---
+++ Assets/Scripts/buttonHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class buttonHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("button pressed "+vb.VirtualButtonName);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("button released " + vb.VirtualButtonName);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < vbs.Length; i++)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/trackableEvent.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class trackableEvent : MonoBehaviour, ITrackableEventHandler
+{
+    private VuMarkManager mVuMarkManager;
+    protected TrackableBehaviour mTrackableBehaviour;
+    private TrackableBehaviour.Status m_PreviousStatus;
+    private TrackableBehaviour.Status m_NewStatus;
+
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName +
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("coucou "+mTrackableBehaviour.name);
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    protected virtual void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+
+        mVuMarkManager = TrackerManager.Instance.GetStateManager().GetVuMarkManager();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var bhvr in mVuMarkManager.GetActiveBehaviours())
+        {
+            if (bhvr.gameObject.Equals(this.gameObject))
+            {
+                TextMesh t = this.gameObject.GetComponentInChildren<TextMesh>();
+                
+                t.text = bhvr.VuMarkTarget.InstanceId.StringValue;
+
+            }
+        }
+        
+    }
+}

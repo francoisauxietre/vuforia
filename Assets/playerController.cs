0d2215f1de8d2dd4540ef44b00f0c551c32ad399
francois-auxietre <f.auxietre@gmail.com>
2020-01-21 23:47:19 +0100

    ajout des vaisseau de navmesh changement du projet en 10,4

---
+++ Assets/playerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent navMeshAgent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            navMeshAgent.SetDestination(raycastHit.point);
+        }
+
+    }
+}
